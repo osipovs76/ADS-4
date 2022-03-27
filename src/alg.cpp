18cedbcd0f565461cacf44edac1eb98d8dae83c2
Osipov Andrey(21-IST5) <94702769+osipovs76@users.noreply.github.com>
2022-03-27 10:39:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,6 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  int count = 0;
+ int count = 0;
   for (int i = 0; i < len-1; i++) {
     for (int j = i+1; j < len; j++) {
       if (arr[i] + arr[j] == value)
